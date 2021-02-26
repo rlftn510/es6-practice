df9b1056db27f9514bd7fdfb55578fb7a3767398
류길수 <rlftn510@naver.com>
2021-02-26 19:42:17 +0900

    proxy 기본

---
+++ proxy/proxy.js
@@ -0,0 +1,17 @@
+// proxy 사전적 의미 : 대리, 대신
+
+const target = {food : '밥'}
+
+const handler = {
+   get(target, key) {
+      return target[key] + ',수저'
+   },
+   set(target, key) {}
+}
+const middle = new Proxy(target, handler)
+
+const left = middle.food
+console.log(left)
+
+const obj = Proxy
+debugger
